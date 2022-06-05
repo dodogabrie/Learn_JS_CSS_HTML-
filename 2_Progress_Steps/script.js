adefce8421e76d34a3eea4e4f3318bc5bd2d4842
dodogabrie <dodogabrie97@live.it>
2022-06-05 11:41:38 +0200

    added second project

---
+++ 2_Progress_Steps/script.js
@@ -0,0 +1,56 @@
+const progress = document.getElementById('progress')
+const prev = document.getElementById('prev')
+const next = document.getElementById('next')
+const circles = document.querySelectorAll('.circle')
+
+let currentActive = 1
+
+circles.forEach((circle, idx)=> {
+    circle.addEventListener(('click'), ()=> {
+      if(idx < currentActive){
+        currentActive = idx + 1 
+        update()
+      }
+    })  
+})
+
+next.addEventListener('click', ()=>{
+  currentActive ++
+
+  if(currentActive > circles.length){
+    currentActive = circles.length
+  }
+  update()
+})
+
+prev.addEventListener('click', ()=>{
+  currentActive --
+
+  if(currentActive < 1){
+    currentActive = 1  
+  }
+  update()
+})
+
+function update() {
+  circles.forEach((circles, idx) => {
+    if(idx < currentActive){
+      circles.classList.add('active')
+    } else {
+      circles.classList.remove('active')
+    }
+  })
+  const actives = document.querySelectorAll('.active')
+  const perc = ((actives.length-1) / (circles.length-1 ))
+  progress.style.width = perc*100 + '%'
+  if(currentActive === 1) {
+    prev.disabled = true
+    next.disabled = false
+  } else if(currentActive === circles.length) {
+    next.disabled = true
+    prev.disabled = false
+  } else {
+    prev.disabled = false
+    next.disables = false
+  }
+}
